35dd561236810e8672a29b90f1306160c4360603
zoraaver <zoraaver@gmail.com>
2021-03-28 19:53:33 +0100

    added navbar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import * as Bootstrap from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+
+export function Navbar() {
+  return (
+    <Bootstrap.Navbar bg="dark" variant="dark">
+      <Bootstrap.Navbar.Brand href="#home">wLogger</Bootstrap.Navbar.Brand>
+      <Bootstrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Nav>
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/logs">Logs</Nav.Link>
+      </Nav>
+      <Nav className="ml-auto">
+        <Nav.Link href="/logout">Logout</Nav.Link>
+      </Nav>
+    </Bootstrap.Navbar>
+  );
+}
