deee20716353438aede6e75f518f8a53a09b9232
zoraaver <zoraaver@gmail.com>
2021-03-29 00:28:25 +0100

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,9 @@ import { App } from "./components/App";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-document.getElementById("root")!.style.height = "100%";
-document.getElementById("root")!.style.width = "100%";
+const root: HTMLDivElement = document.getElementById("root") as HTMLDivElement;
+root.style.height = "100%";
+root.style.width = "100%";
 
 ReactDOM.render(
   <Router>
