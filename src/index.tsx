ffa91769285c8c68681854f9d2ca1d0e9b2c5986
zoraaver <zoraaver@gmail.com>
2021-03-28 19:52:28 +0100

    added 100% height + width for main root div

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./components/App";
 import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+document.getElementById("root")!.style.height = "100%";
+document.getElementById("root")!.style.width = "100%";
 
 ReactDOM.render(
   <Router>
