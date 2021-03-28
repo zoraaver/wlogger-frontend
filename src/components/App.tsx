21b06c03b7e0dbd934fce7a59b10f24e85d4912a
zoraaver <zoraaver@gmail.com>
2021-03-28 16:52:01 +0100

    added routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,17 @@
 import * as React from "react";
+import { Route, Switch } from "react-router-dom";
+import { HomePage } from "../pages/HomePage";
+import { LoginPage } from "../pages/LoginPage";
 
 export function App() {
-  return <h1>wLogger app</h1>;
+  return (
+    <Switch>
+      <Route exact path="/login">
+        <LoginPage />
+      </Route>
+      <Route exact path="/">
+        <HomePage />
+      </Route>
+    </Switch>
+  );
 }
