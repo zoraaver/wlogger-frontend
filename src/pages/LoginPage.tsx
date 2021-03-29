0f0ffbdc83801db968f5d2b47de5e3600e2be36a
zoraaver <zoraaver@gmail.com>
2021-03-29 11:44:50 +0100

    moved login form to new component

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,61 +1,27 @@
 import * as React from "react";
-import { ChangeEvent, useState } from "react";
-import { Button, Form, Col, Row, Container } from "react-bootstrap";
-import { GoogleButton } from "../components/GoogleButton";
+import { Container } from "react-bootstrap";
+import { LoginForm } from "../components/LoginForm";
 
 export function LoginPage() {
-  const [formData, setFormData] = useState({ email: "", password: "" });
-
-  function handleChange(event: ChangeEvent<HTMLInputElement>): void {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
-  }
-
   return (
     <Container
       fluid
       className="vh-100"
       style={{
         backgroundColor: "powderblue",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "start",
+        display: "inline-flex",
+        alignItems: "center",
       }}
     >
-      <h1
-        className="display-3"
-        style={{ fontFamily: "Monaco", paddingTop: "50px" }}
-      >
-        <strong>Track and log workouts</strong>
-      </h1>
-      <Form className="shadow p-4 mt-5 w-50 rounded bg-light bg-gradient">
-        <Row>
-          <Col>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                onChange={handleChange}
-                type="email"
-                placeholder="Enter email"
-              />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                onChange={handleChange}
-                type="password"
-                placeholder="Password"
-              />
-            </Form.Group>
-            <Button className="py-1" variant="primary" type="submit">
-              Login
-            </Button>
-          </Col>
-          <div style={{ borderLeft: "4px solid lightgrey" }}></div>
-          <Col style={{ display: "flex", alignItems: "center" }}>
-            <GoogleButton />
-          </Col>
-        </Row>
-      </Form>
+      <div style={{ fontFamily: "Balsamiq Sans" }} className="ml-5">
+        <h1 className="display-2">
+          <strong>wLogger</strong>
+        </h1>
+        <h1 className="display-3" style={{ paddingTop: "50px" }}>
+          <strong>Track and log workouts.</strong>
+        </h1>
+      </div>
+      <LoginForm />
     </Container>
   );
 }
