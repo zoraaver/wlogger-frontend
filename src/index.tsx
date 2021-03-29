285d39161bbb4e920f65e1ebe23ebef58a0df5a6
zoraaver <zoraaver@gmail.com>
2021-03-29 18:57:23 +0100

    added redux imports

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,36 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./components/App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { usersReducer } from "./slices/usersSlice";
+import {
+  Provider,
+  TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 const root: HTMLDivElement = document.getElementById("root") as HTMLDivElement;
 root.style.height = "100%";
 root.style.width = "100%";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
