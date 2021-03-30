e902f3bc1a963f425c7d09bde3ec0e1f31722803
zoraaver <zoraaver@gmail.com>
2021-03-30 19:41:10 +0100

    added cases for fulfilled and rejected login

--- src/slices/usersSlice.ts
+++ src/slices/usersSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import axios, { AxiosResponse } from "axios";
 
 const BACKEND_URL = "http://localhost:8080";
@@ -14,21 +14,50 @@ export const loginUser = createAsyncThunk(
   "users/loginUser",
   async (formData: userFormData) => {
     try {
-      const response: AxiosResponse<any> = await axios.post(
+      const response: AxiosResponse<{ user: userData }> = await axios.post(
         LOGIN_URL,
         formData
       );
-      console.log(response.data);
+      localStorage.setItem("wloggerUserToken", response.data.user.token);
+      return response.data.user;
     } catch (error) {
-      console.log(error.message);
+      return Promise.reject(error.response.data);
     }
   }
 );
 
+interface userData {
+  email: string;
+  token: string;
+}
+
+interface userState {
+  loginError: string | undefined;
+  data: userData | null;
+}
+
+const initialState: userState = {
+  data: null,
+  loginError: undefined,
+};
+
 export const slice = createSlice({
-  name: "users",
-  initialState: { user: null },
+  name: "user",
+  initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(loginUser.rejected, (state, action) => {
+      state.data = null;
+      state.loginError = action.error.message;
+    });
+    builder.addCase(
+      loginUser.fulfilled,
+      (state, action: PayloadAction<userData>) => {
+        const userData = action.payload;
+        state.data = userData;
+      }
+    );
+  },
 });
 
-export const usersReducer = slice.reducer;
+export const userReducer = slice.reducer;
