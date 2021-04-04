58e1ffe63a2849fabf756b40f6451f63e38781c5
zoraaver <zoraaver@gmail.com>
2021-04-04 22:51:14 +0100

    added verify action

--- src/slices/usersSlice.ts
+++ src/slices/usersSlice.ts
@@ -6,15 +6,26 @@ const BASE_URL = BACKEND_URL;
 const LOGIN_URL = BASE_URL + "/auth/login";
 const GOOGLE_LOGIN_URL = BASE_URL + "/auth/google";
 const VALIDATE_URL = BASE_URL + "/auth/validate";
+const VERIFY_URL = BASE_URL + "/auth/verify";
+const USERS_URL = BASE_URL + "/users";
 
-interface userFormData {
+interface userLoginData {
   email: string;
   password: string;
 }
 
+type userSignupData = userLoginData & {
+  confirmPassword: string;
+};
+
+interface userData {
+  email: string;
+  token: string;
+}
+
 export const loginUser = createAsyncThunk(
   "users/loginUser",
-  async (formData: userFormData) => {
+  async (formData: userLoginData) => {
     try {
       const response: AxiosResponse<{ user: userData }> = await axios.post(
         LOGIN_URL,
@@ -45,7 +56,7 @@ export const googleLoginUser = createAsyncThunk(
   }
 );
 
-export const validate = createAsyncThunk("users/validate", async () => {
+export const validateUser = createAsyncThunk("users/validate", async () => {
   try {
     const response: AxiosResponse<{
       user: userData;
@@ -59,13 +70,50 @@ export const validate = createAsyncThunk("users/validate", async () => {
   }
 });
 
-interface userData {
-  email: string;
-  token: string;
+export const verifyUser = createAsyncThunk(
+  "users/verify",
+  async (verificationToken: string) => {
+    try {
+      const response: AxiosResponse<{ user: userData }> = await axios.post(
+        VERIFY_URL,
+        {
+          verificationToken,
+        }
+      );
+      localStorage.setItem("token", response.data.user.token);
+      return response.data.user;
+    } catch (error) {
+      if (error.response) return Promise.reject(error.response.data);
+      return Promise.reject(error);
+    }
+  }
+);
+
+export const signupUser = createAsyncThunk(
+  "users/signupUser",
+  async (formData: userSignupData, { rejectWithValue }) => {
+    try {
+      const response: AxiosResponse<{
+        user: { email: string };
+      }> = await axios.post(USERS_URL, formData);
+      return response.data.user.email;
+    } catch (error) {
+      if (error.response) return rejectWithValue(error.response.data);
+      return Promise.reject(error);
+    }
+  }
+);
+
+interface signupError {
+  field: string;
+  error: string;
 }
 
 interface userState {
   loginError: string | undefined;
+  signupError: signupError | undefined;
+  signupSuccess: string | undefined;
+  verificationError: string | undefined;
   authenticationStatus: "pending" | "confirmed" | "unknown";
   data: userData | null;
 }
@@ -73,13 +121,21 @@ interface userState {
 const initialState: userState = {
   data: null,
   loginError: undefined,
+  verificationError: undefined,
+  signupSuccess: undefined,
+  signupError: undefined,
   authenticationStatus: "unknown",
 };
 
-export const slice = createSlice({
+const slice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    logoutUser(state, action: PayloadAction<void>) {
+      state.data = null;
+      state.authenticationStatus = "unknown";
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(loginUser.rejected, (state, action) => {
       state.data = null;
@@ -91,6 +147,7 @@ export const slice = createSlice({
       (state, action: PayloadAction<userData>) => {
         const userData = action.payload;
         state.data = userData;
+        state.loginError = undefined;
         state.authenticationStatus = "confirmed";
       }
     );
@@ -102,6 +159,7 @@ export const slice = createSlice({
       (state, action: PayloadAction<userData>) => {
         const userData = action.payload;
         state.data = userData;
+        state.loginError = undefined;
         state.authenticationStatus = "confirmed";
       }
     );
@@ -114,21 +172,52 @@ export const slice = createSlice({
       state.authenticationStatus = "pending";
     });
     builder.addCase(
-      validate.fulfilled,
+      validateUser.fulfilled,
       (state, action: PayloadAction<userData>) => {
         const userData = action.payload;
         state.data = userData;
         state.authenticationStatus = "confirmed";
       }
     );
-    builder.addCase(validate.pending, (state, action) => {
+    builder.addCase(validateUser.pending, (state, action) => {
       state.authenticationStatus = "pending";
     });
-    builder.addCase(validate.rejected, (state, action) => {
+    builder.addCase(validateUser.rejected, (state, action) => {
       state.data = null;
       state.authenticationStatus = "unknown";
     });
+    builder.addCase(
+      verifyUser.fulfilled,
+      (state, action: PayloadAction<userData>) => {
+        const userData = action.payload;
+        state.data = userData;
+        state.authenticationStatus = "confirmed";
+      }
+    );
+    builder.addCase(verifyUser.rejected, (state, action) => {
+      state.data = null;
+      state.verificationError = action.error.message;
+      state.authenticationStatus = "unknown";
+    });
+    builder.addCase(
+      signupUser.fulfilled,
+      (state, action: PayloadAction<string>) => {
+        state.signupSuccess = `Account successfully created: a verification email has been sent to ${action.payload}`;
+      }
+    );
+    builder.addCase(
+      signupUser.rejected,
+      (state, action: PayloadAction<unknown>) => {
+        const signupError = action.payload as signupError;
+        state.signupError = {
+          field: signupError.field,
+          error: signupError.error,
+        };
+        state.signupSuccess = undefined;
+      }
+    );
   },
 });
 
+export const { logoutUser } = slice.actions;
 export const userReducer = slice.reducer;
