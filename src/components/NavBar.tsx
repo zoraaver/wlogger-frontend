0c78890349c5e41cd936573a845e8ca4606aa0be
zoraaver <zoraaver@gmail.com>
2021-04-04 22:50:36 +0100

    added conditional rendering of links for login status

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,24 @@
 import * as React from "react";
 import * as Bootstrap from "react-bootstrap";
 import { Nav } from "react-bootstrap";
+import { useAppDispatch, useAppSelector } from "..";
+import { useHistory } from "react-router-dom";
+import { logoutUser } from "../slices/usersSlice";
 
 export function Navbar() {
+  const dispatch = useAppDispatch();
+  const authenticationStatus = useAppSelector(
+    (state) => state.user.authenticationStatus
+  );
+  const authenticated = authenticationStatus === "confirmed";
+  const history = useHistory();
+
+  function logout() {
+    localStorage.clear();
+    dispatch(logoutUser());
+    history.push("/login");
+  }
+
   return (
     <Bootstrap.Navbar bg="info" fixed="top" variant="dark">
       <Bootstrap.Navbar.Brand
@@ -12,13 +28,19 @@ export function Navbar() {
         wLogger
       </Bootstrap.Navbar.Brand>
       <Bootstrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Nav>
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/logs">Logs</Nav.Link>
-      </Nav>
-      <Nav className="ml-auto">
-        <Nav.Link href="/logout">Logout</Nav.Link>
-      </Nav>
+      {authenticated ? (
+        <Nav>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/logs">Logs</Nav.Link>
+        </Nav>
+      ) : null}
+      {authenticated ? (
+        <Nav className="ml-auto">
+          <Nav.Link onClick={logout} as="button" className="btn">
+            Logout
+          </Nav.Link>
+        </Nav>
+      ) : null}
     </Bootstrap.Navbar>
   );
 }
