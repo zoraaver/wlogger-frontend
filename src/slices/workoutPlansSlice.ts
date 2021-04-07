88b7e3a12303b75c8639562fab2ea06b7c761f09
zoraaver <zoraaver@gmail.com>
2021-04-07 13:52:46 +0100

    added delete actions for workouts/weeks/exercises

--- src/slices/workoutPlansSlice.ts
+++ src/slices/workoutPlansSlice.ts
@@ -130,6 +130,73 @@ const slice = createSlice({
         }
       }
     },
+    deleteExercise(
+      state,
+      action: PayloadAction<{
+        weekPosition: number;
+        day: Day;
+        exerciseIndex: number;
+      }>
+    ) {
+      const { weekPosition, day, exerciseIndex } = action.payload;
+      const week = state.editWorkoutPlan?.weeks.find(
+        (week: weekData) => week.position === weekPosition
+      );
+      if (week !== undefined) {
+        const workout = week.workouts.find(
+          (workout: workoutData) => workout.dayOfWeek === day
+        );
+        if (workout !== undefined) {
+          workout.exercises.splice(exerciseIndex, 1);
+        }
+      }
+    },
+    deleteWeek(state, action: PayloadAction<number>) {
+      const position: number = action.payload;
+      if (!state.editWorkoutPlan) return;
+      const weeks = state.editWorkoutPlan.weeks;
+      const weekToDeleteIndex = weeks.findIndex(
+        (week: weekData) => week.position === position
+      );
+      if (weekToDeleteIndex !== undefined && weekToDeleteIndex >= 0) {
+        weeks.forEach((week: weekData) => {
+          if (week.position > weeks[weekToDeleteIndex].position) {
+            --week.position;
+          }
+        });
+        state.editWorkoutPlan?.weeks.splice(weekToDeleteIndex, 1);
+      }
+    },
+    deleteWorkout(
+      state,
+      action: PayloadAction<{ position: number; day: Day }>
+    ) {
+      if (!state.editWorkoutPlan) return;
+      const { position, day } = action.payload;
+      const week = state.editWorkoutPlan.weeks.find(
+        (week: weekData) => week.position === position
+      );
+      if (week !== undefined) {
+        const workoutToDeleteIndex = week.workouts.findIndex(
+          (workout: workoutData) => workout.dayOfWeek === day
+        );
+        if (workoutToDeleteIndex !== undefined && workoutToDeleteIndex >= 0) {
+          week.workouts.splice(workoutToDeleteIndex, 1);
+        }
+      }
+    },
+    deleteEmptyWorkouts(state, action: PayloadAction<number>) {
+      if (!state.editWorkoutPlan) return;
+      const postion: number = action.payload;
+      const week = state.editWorkoutPlan.weeks.find(
+        (week: weekData) => week.position === postion
+      );
+      if (week !== undefined) {
+        week.workouts = week.workouts.filter(
+          (workout: workoutData) => workout.exercises.length !== 0
+        );
+      }
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(postWorkoutPlan.fulfilled, (state, action) => {
@@ -144,4 +211,8 @@ export const {
   addWeek,
   addWorkout,
   addExercise,
+  deleteExercise,
+  deleteWeek,
+  deleteWorkout,
+  deleteEmptyWorkouts,
 } = slice.actions;
