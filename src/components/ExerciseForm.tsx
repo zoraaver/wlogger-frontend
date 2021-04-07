d815f9111721f2fb54d78a621db910f5f30932ee
zoraaver <zoraaver@gmail.com>
2021-04-07 13:51:06 +0100

    added (client-side) validations for form entries

--- src/components/ExerciseForm.tsx
+++ src/components/ExerciseForm.tsx
@@ -11,21 +11,36 @@ interface ExerciseFormProps {
 }
 export function ExerciseForm({ dayOfWeek, position }: ExerciseFormProps) {
   const [formData, setFormData] = React.useState<exerciseData>({
-    name: "",
+    name: "Squats",
     sets: 1,
     weight: 10,
     repetitions: 10,
     unit: "kg",
     restInterval: 0,
   });
+  const [error, setError] = React.useState({ field: "", message: "" });
+
   function handleChange({ target }: React.ChangeEvent<HTMLInputElement>) {
-    setFormData({ ...formData, [target.name]: target.value });
+    if (target.name === "name" || target.name === "unit") {
+      setFormData({ ...formData, [target.name]: target.value });
+    } else {
+      setFormData({ ...formData, [target.name]: Number(target.value) });
+    }
   }
 
   const dispatch = useAppDispatch();
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
-    dispatch(addExercise({ position, day: dayOfWeek, exerciseData: formData }));
+    if (formData.name === "") {
+      setError({ field: "name", message: "Name is a required field." });
+    } else if (formData.sets === 0) {
+      setError({ field: "sets", message: "No. of sets cannot be 0" });
+    } else {
+      setError({ field: "", message: "" });
+      dispatch(
+        addExercise({ position, day: dayOfWeek, exerciseData: formData })
+      );
+    }
   }
 
   return (
@@ -39,6 +54,9 @@ export function ExerciseForm({ dayOfWeek, position }: ExerciseFormProps) {
             onChange={handleChange}
             placeholder="exercise name"
           />
+          {error.field === "name" ? (
+            <div className="text-danger">{error.message}</div>
+          ) : null}
         </Col>
         <Col>
           <Form.Label>Sets</Form.Label>
@@ -48,6 +66,9 @@ export function ExerciseForm({ dayOfWeek, position }: ExerciseFormProps) {
             value={formData.sets}
             placeholder="sets"
           />
+          {error.field === "sets" ? (
+            <div className="text-danger">{error.message}</div>
+          ) : null}
         </Col>
         <Col>
           <Form.Label>Reps</Form.Label>
