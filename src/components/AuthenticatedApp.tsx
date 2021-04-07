4a2b0a9af06c8bd5e8c4f21556faef86c6c040c2
zoraaver <zoraaver@gmail.com>
2021-04-07 19:15:08 +0100

    added routes for workout plans

--- src/components/AuthenticatedApp.tsx
+++ src/components/AuthenticatedApp.tsx
@@ -7,6 +7,7 @@ import { Sidebar } from "./Sidebar/Sidebar";
 import { NewWorkoutPlanPage } from "../pages/NewWorkoutPlanPage";
 import { EditWorkoutPlanPage } from "../pages/EditWorkoutPlanPage";
 import { EditWeekPage } from "../pages/EditWeekPage";
+import { WorkoutPlansPage } from "../pages/WorkoutPlansPage";
 
 export function AuthenticatedApp() {
   return (
@@ -14,20 +15,29 @@ export function AuthenticatedApp() {
       <Navbar />
       <Sidebar />
       <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
         <Route exact path="/login">
           <Redirect to="/" />
         </Route>
         <Route exact path="/plans/new/weeks/:position">
           <EditWeekPage />
         </Route>
+        <Route exact path="/plans/:id/weeks/:position">
+          <EditWeekPage />
+        </Route>
         <Route exact path="/plans/new/weeks">
+          <EditWorkoutPlanPage />
+        </Route>
+        <Route exact path="/plans/:id/weeks">
           <EditWorkoutPlanPage />
         </Route>
         <Route exact path="/plans/new">
           <NewWorkoutPlanPage />
+        </Route>
+        <Route exact path="/plans">
+          <WorkoutPlansPage />
+        </Route>
+        <Route exact path="/">
+          <HomePage />
         </Route>
         <Route path="*">
           <NotFoundPage />
