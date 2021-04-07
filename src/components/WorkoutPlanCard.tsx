462310bf90b90896472c762feb1cc18e375a925d
zoraaver <zoraaver@gmail.com>
2021-04-07 19:14:03 +0100

    added manage plans page

---
+++ src/components/WorkoutPlanCard.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { Pencil, Trash } from "react-bootstrap-icons";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { workoutPlanHeaderData } from "../slices/workoutPlansSlice";
+
+type WorkoutPlanCardProps = workoutPlanHeaderData & {
+  handleShow: (_id: string, name: string) => void;
+};
+
+export function WorkoutPlanCard({
+  name,
+  length,
+  _id,
+  handleShow,
+}: WorkoutPlanCardProps) {
+  return (
+    <Card className="w-50 mt-4">
+      <Card.Header className="d-flex flex-row justify-content-end align-items-center">
+        <Card.Title className="mb-0 mr-auto">{name}</Card.Title>
+        <Button
+          variant="link"
+          href={`/plans/${_id}/weeks`}
+          className="mb-0 d-inline-block"
+        >
+          <Pencil />
+        </Button>
+        <Button
+          onClick={() => handleShow(_id, name)}
+          variant="danger"
+          className="ml-2 mb-0 d-inline-block"
+        >
+          <Trash />
+        </Button>
+      </Card.Header>
+      <Card.Body>
+        <strong>Length:</strong> {length === 0 ? "Indefinite" : length} weeks
+      </Card.Body>
+    </Card>
+  );
+}
---
+++ src/pages/WorkoutPlansPage.tsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+import Container from "react-bootstrap/Container";
+import { useAppDispatch, useAppSelector } from "..";
+import { WorkoutPlanCard } from "../components/WorkoutPlanCard";
+import {
+  deleteWorkoutPlan,
+  getWorkoutPlans,
+  workoutPlanHeaderData,
+} from "../slices/workoutPlansSlice";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+export function WorkoutPlansPage() {
+  const dispatch = useAppDispatch();
+  const [show, setShow] = React.useState(false);
+  const [planToDelete, setPlanToDelete] = React.useState({ name: "", _id: "" });
+
+  function handleShow(_id: string, name: string) {
+    setPlanToDelete({ _id, name });
+    setShow(true);
+  }
+
+  function handleDeleteClick() {
+    dispatch(deleteWorkoutPlan(planToDelete._id));
+    setShow(false);
+  }
+
+  const workoutPlans: workoutPlanHeaderData[] = useAppSelector(
+    (state) => state.workoutPlans.data
+  );
+
+  React.useEffect(() => {
+    dispatch(getWorkoutPlans());
+  }, []);
+
+  return (
+    <Container
+      className="d-flex flex-column justify-content-center align-items-center"
+      style={{ marginLeft: 200, marginTop: 68 }}
+    >
+      <h2>My plans</h2>
+      {workoutPlans.map((workoutPlan: workoutPlanHeaderData) => (
+        <WorkoutPlanCard
+          key={workoutPlan._id}
+          name={workoutPlan.name}
+          length={workoutPlan.length}
+          _id={workoutPlan._id}
+          handleShow={handleShow}
+        />
+      ))}
+      <Modal show={show} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          Are you sure you want to delete{" "}
+          <strong className="ml-1">{planToDelete.name}</strong>?
+        </Modal.Header>
+        <Modal.Body>This action is irreversible.</Modal.Body>
+        <Modal.Footer className="py-1">
+          <Button
+            variant="primary"
+            className="py-1"
+            onClick={() => setShow(false)}
+          >
+            Close
+          </Button>
+          <Button variant="danger" className="py-1" onClick={handleDeleteClick}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
+  );
+}
