f62fdd0d186f84ee0e0e00a56113a941ecc984c0
zoraaver <zoraaver@gmail.com>
2021-04-13 17:21:28 +0100

    temporarily remove timer

--- src/containers/NavBar.tsx
+++ src/containers/NavBar.tsx
@@ -2,9 +2,9 @@ import * as React from "react";
 import * as Bootstrap from "react-bootstrap";
 import { Nav } from "react-bootstrap";
 import { useAppDispatch, useAppSelector } from "..";
-import { useHistory, useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { logoutUser } from "../slices/usersSlice";
-import { Timer } from "../components/Timer";
+// import { Timer } from "../components/Timer";
 
 export function Navbar() {
   const dispatch = useAppDispatch();
@@ -13,8 +13,8 @@ export function Navbar() {
   );
   const authenticated = authenticationStatus === "confirmed";
   const history = useHistory();
-  const location = useLocation();
-  const onNewWorkoutLogPage = location.pathname === "/logs/new";
+  // const location = useLocation();
+  // const onNewWorkoutLogPage = location.pathname === "/logs/new";
 
   function logout() {
     localStorage.clear();
@@ -37,7 +37,7 @@ export function Navbar() {
           <Nav.Link href="/logs">Logs</Nav.Link>
         </Nav>
       ) : null}
-      {onNewWorkoutLogPage ? <Timer title="Elapsed time" /> : null}
+      {/* {onNewWorkoutLogPage ? <Timer title="Workout time" /> : null} */}
       {authenticated ? (
         <Nav className="ml-auto">
           <Nav.Link onClick={logout} as="button" className="btn">
