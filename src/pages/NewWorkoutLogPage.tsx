547be9aa19d3c840c21fc11b36cf2d7364811271
zoraaver <zoraaver@gmail.com>
2021-04-13 17:22:06 +0100

    added button to post new workout log

--- src/pages/NewWorkoutLogPage.tsx
+++ src/pages/NewWorkoutLogPage.tsx
@@ -1,11 +1,29 @@
 import * as React from "react";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import { WorkoutLogForm } from "../components/WorkoutLogForm";
+import { WorkoutLogTable } from "../containers/WorkoutLogTable";
+import { useAppDispatch, useAppSelector } from "..";
+import { postWorkoutLog } from "../slices/workoutLogsSlice";
 
 export function NewWorkoutLogPage() {
+  const workoutLog = useAppSelector(
+    (state) => state.workoutLogs.editWorkoutLog
+  );
+  const dispatch = useAppDispatch();
+
+  function handleSubmit() {
+    if (workoutLog) {
+      dispatch(postWorkoutLog(workoutLog));
+    }
+  }
   return (
     <Container className="mt-5 d-flex flex-column justify-content-start align-items-center">
       <WorkoutLogForm />
+      <WorkoutLogTable />
+      <Button variant="success" onClick={handleSubmit}>
+        Log workout
+      </Button>
     </Container>
   );
 }
