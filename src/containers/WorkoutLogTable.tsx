f3ba6b6e20baa37ce60f1eb2ffe368bb663357f4
zoraaver <zoraaver@gmail.com>
2021-04-13 17:21:42 +0100

    finished table

--- src/containers/WorkoutLogTable.tsx
+++ src/containers/WorkoutLogTable.tsx
@@ -1,5 +1,44 @@
 import * as React from "react";
+import Table from "react-bootstrap/Table";
+import { useAppSelector } from "..";
+import { exerciseLogData, setLogData } from "../slices/workoutLogsSlice";
+import { renderRestInterval } from "../util/util";
 
 export function WorkoutLogTable() {
-  return <h1>Table</h1>;
+  const exercises: exerciseLogData[] | undefined = useAppSelector(
+    (state) => state.workoutLogs.editWorkoutLog?.exercises
+  );
+
+  if (!exercises) return <></>;
+
+  function renderRows() {
+    return exercises?.map((exercise: exerciseLogData, index: number) =>
+      exercise.sets.map((set: setLogData, index: number) => (
+        <tr key={index}>
+          {index === 0 ? (
+            <td rowSpan={exercise.sets.length}>{exercise.name}</td>
+          ) : null}
+          <td>{set.repetitions}</td>
+          <td>
+            {set.weight} {set.unit}
+          </td>
+          <td>{renderRestInterval(set.restInterval)}</td>
+        </tr>
+      ))
+    );
+  }
+
+  return (
+    <Table striped bordered className="mt-4 w-75" variant="dark">
+      <thead>
+        <tr>
+          <th>Exercise</th>
+          <th>Repetitions</th>
+          <th>Weight</th>
+          <th>Rest Interval (mm:ss)</th>
+        </tr>
+      </thead>
+      <tbody>{renderRows()}</tbody>
+    </Table>
+  );
 }
