cfa9a37c92b5f57efc9897d114b9343b58a0d18d
zoraaver <zoraaver@gmail.com>
2021-04-14 17:27:34 +0100

    added delete buttons

--- src/pages/WorkoutLogsPage.tsx
+++ src/pages/WorkoutLogsPage.tsx
@@ -1,15 +1,18 @@
 import * as React from "react";
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import { useAppDispatch, useAppSelector } from "..";
 import {
+  deleteWorkoutLog,
   getWorkoutLogs,
+  resetSuccess,
   workoutLogHeaderData,
 } from "../slices/workoutLogsSlice";
 import { ListGroup } from "react-bootstrap";
-import { Trash } from "react-bootstrap-icons";
 import { DeleteModal } from "../components/DeleteModal";
+import { WorkoutLogItem } from "../components/WorkoutLogItem";
 
 export function WorkoutLogsPage() {
   const workoutLogs: workoutLogHeaderData[] = useAppSelector(
@@ -20,12 +23,35 @@ export function WorkoutLogsPage() {
   );
   const dispatch = useAppDispatch();
   const [show, setShow] = React.useState(false);
+  const [logToDelete, setLogToDelete] = React.useState({
+    _id: "",
+    createdAt: new Date(),
+  });
 
   React.useEffect(() => {
     dispatch(getWorkoutLogs());
   }, []);
 
-  function handleDeleteClick() {}
+  function handleDeleteClick() {
+    dispatch(deleteWorkoutLog(logToDelete._id));
+    dispatch(resetSuccess(4));
+    setShow(false);
+  }
+
+  function handleShow(
+    _id: workoutLogHeaderData["_id"],
+    createdAt: workoutLogHeaderData["createdAt"]
+  ) {
+    setShow(true);
+    setLogToDelete({ _id, createdAt: new Date(createdAt) });
+  }
+
+  const modalTitle: JSX.Element = (
+    <>
+      Are you sure you want to delete the log created at{" "}
+      {logToDelete.createdAt.toLocaleString()}?
+    </>
+  );
 
   return (
     <Container className="mt-5 d-flex flex-column justify-content-start align-items-center">
@@ -33,44 +59,34 @@ export function WorkoutLogsPage() {
       {successMessage ? (
         <Alert variant="success">{successMessage}</Alert>
       ) : null}
-      <ListGroup variant="flush" className="w-50">
-        {workoutLogs.map((workoutLog) => {
-          const date = new Date(workoutLog.createdAt as string);
-          return (
-            <ListGroup.Item
+      {workoutLogs.length === 0 ? (
+        <Card className="mt-3 w-50 text-center">
+          <Card.Header>
+            Looks like you haven't logged any workouts yet!
+          </Card.Header>
+          <Card.Body>
+            <Button className="py-1" href="/logs/new">
+              Log a workout
+            </Button>
+          </Card.Body>
+        </Card>
+      ) : (
+        <ListGroup variant="flush" className="w-50">
+          {workoutLogs.map((workoutLog) => (
+            <WorkoutLogItem
+              workoutLog={workoutLog}
+              handleShow={handleShow}
               key={workoutLog._id}
-              className="d-flex flex-row justify-content-start align-items-center"
-            >
-              <h5 className="my-0  mx-3 d-inline-block">
-                {date.toDateString()}:
-              </h5>{" "}
-              {workoutLog.exerciseCount} exercise
-              {workoutLog.exerciseCount > 1 ? "s" : null}, {workoutLog.setCount}{" "}
-              set{workoutLog.setCount > 1 ? "s" : null}
-              <Button
-                variant="link"
-                className="ml-auto my-0 py-0"
-                href={"logs/" + workoutLog._id}
-              >
-                View
-              </Button>
-              <Button
-                onClick={() => setShow(true)}
-                variant="danger"
-                className="py-1 px-1"
-              >
-                <Trash />
-              </Button>
-            </ListGroup.Item>
-          );
-        })}
-      </ListGroup>
+            />
+          ))}
+        </ListGroup>
+      )}
       <DeleteModal
         handleCloseClick={() => setShow(false)}
         handleDeleteClick={handleDeleteClick}
         onHide={() => setShow(false)}
         show={show}
-        title={"never"}
+        title={modalTitle}
       />
     </Container>
   );
