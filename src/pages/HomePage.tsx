b583e04fdc84c5050422c19d461a9e192361e280
zoraaver <zoraaver@gmail.com>
2021-04-14 17:26:36 +0100

    added top margin to title

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ import Button from "react-bootstrap/Button";
 export function HomePage() {
   return (
     <Container className="mt-5 d-flex flex-column justify-content-start align-items-center">
-      <h1>Homepage</h1>
+      <h1 className="mt-3">Homepage</h1>
       <Button variant="primary" href="/logs/new">
         Log workout
       </Button>
