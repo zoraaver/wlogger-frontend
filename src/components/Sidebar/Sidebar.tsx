0cd85598eb43f26c0ead8b41cc5acec9f031a4c7
zoraaver <zoraaver@gmail.com>
2021-04-21 18:10:53 +0100

    change links -> buttons to preserve sidebar state across pages

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,43 @@
 import * as React from "react";
-import { Nav } from "react-bootstrap";
+import { useAppSelector } from "../..";
+import Nav from "react-bootstrap/Nav";
 import "./Sidebar.css";
+import { useHistory } from "react-router";
 
 export function Sidebar() {
+  const sidebarOpen: boolean = useAppSelector((state) => state.UI.sidebarOpen);
+  const closed = { display: "none" };
+  const history = useHistory();
   return (
-    <div className="sidenav">
-      <Nav.Link href="/plans/new">Create a new plan</Nav.Link>
-      <Nav.Link href="/plans">Manage my plans</Nav.Link>
-      <Nav.Link href="/statistics">Statistics</Nav.Link>
-      <Nav.Link href="/exercises">Exercises</Nav.Link>
+    <div className="sidenav" style={sidebarOpen ? {} : closed}>
+      <Nav.Link
+        as="button"
+        className="btn"
+        onClick={() => history.push("/plans/new")}
+      >
+        Create a new plan
+      </Nav.Link>
+      <Nav.Link
+        as="button"
+        className="btn"
+        onClick={() => history.push("/plans")}
+      >
+        Manage my plans
+      </Nav.Link>
+      <Nav.Link
+        as="button"
+        className="btn"
+        onClick={() => history.push("/statistics")}
+      >
+        Statistics
+      </Nav.Link>
+      <Nav.Link
+        as="button"
+        className="btn"
+        onClick={() => history.push("/exercises")}
+      >
+        Exercises
+      </Nav.Link>
     </div>
   );
 }
--- src/containers/NavBar.tsx
+++ src/containers/NavBar.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import * as Bootstrap from "react-bootstrap";
-import { Nav } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
 import { useAppDispatch, useAppSelector } from "..";
 import { useHistory } from "react-router-dom";
 import { logoutUser } from "../slices/usersSlice";
-// import { Timer } from "../components/Timer";
+import { LayoutSidebar } from "react-bootstrap-icons";
+import { toggleSidebar } from "../slices/UISlice";
 
 export function Navbar() {
   const dispatch = useAppDispatch();
@@ -13,8 +14,6 @@ export function Navbar() {
   );
   const authenticated = authenticationStatus === "confirmed";
   const history = useHistory();
-  // const location = useLocation();
-  // const onNewWorkoutLogPage = location.pathname === "/logs/new";
 
   function logout() {
     localStorage.clear();
@@ -24,17 +23,23 @@ export function Navbar() {
 
   return (
     <Bootstrap.Navbar bg="info" fixed="top" variant="dark">
-      <Bootstrap.Navbar.Brand
-        href="#home"
-        style={{ fontFamily: "Balsamiq Sans" }}
-      >
+      {authenticated ? (
+        <Nav.Link
+          as="button"
+          onClick={() => dispatch(toggleSidebar())}
+          className="btn p-2 mr-2"
+        >
+          <LayoutSidebar />
+        </Nav.Link>
+      ) : null}
+      <Bootstrap.Navbar.Brand style={{ fontFamily: "Balsamiq Sans" }}>
         wLogger
       </Bootstrap.Navbar.Brand>
       <Bootstrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
       {authenticated ? (
         <Nav>
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/logs">Logs</Nav.Link>
+          <Nav.Link onClick={() => history.push("/")}>Home</Nav.Link>
+          <Nav.Link onClick={() => history.push("/logs")}>Logs</Nav.Link>
         </Nav>
       ) : null}
       {/* {onNewWorkoutLogPage ? <Timer title="Workout time" /> : null} */}
