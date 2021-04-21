14ad732ddd1f643a17260cc385f0976528c18cea
zoraaver <zoraaver@gmail.com>
2021-04-21 18:11:23 +0100

    get next workout

---
+++ src/slices/workoutsSlice.ts
@@ -0,0 +1,67 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AxiosResponse } from "axios";
+import { API } from "../config/axios.config";
+import { weightUnit, Day, workoutPlansUrl } from "./workoutPlansSlice";
+
+export interface exerciseData {
+  name: string;
+  restInterval?: number;
+  sets: number;
+  repetitions?: number;
+  weight?: number;
+  unit: weightUnit;
+}
+
+export interface workoutData {
+  dayOfWeek: Day;
+  date?: string;
+  exercises: Array<exerciseData>;
+}
+
+export const getNextWorkout = createAsyncThunk(
+  "workouts/getNextWorkout",
+  async () => {
+    try {
+      const response: AxiosResponse<workoutData | string> = await API.get(
+        `${workoutPlansUrl}/nextWorkout`
+      );
+      return response.data;
+    } catch (error) {
+      if (error.response) return Promise.reject(error.response.data);
+      return Promise.reject(error.response);
+    }
+  }
+);
+
+interface workoutState {
+  nextWorkout?: workoutData;
+  message?: string;
+}
+
+const initialState: workoutState = {};
+
+const slice = createSlice({
+  initialState,
+  name: "workouts",
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(
+      getNextWorkout.fulfilled,
+      (state, action: PayloadAction<workoutData | string>) => {
+        if (typeof action.payload === "string") {
+          state.message = action.payload;
+          state.nextWorkout = undefined;
+        } else {
+          state.nextWorkout = action.payload;
+          state.message = undefined;
+        }
+      }
+    );
+    builder.addCase(getNextWorkout.rejected, (state, action) => {
+      state.nextWorkout = undefined;
+      state.message = action.error.message;
+    });
+  },
+});
+
+export const workoutsReducer = slice.reducer;
