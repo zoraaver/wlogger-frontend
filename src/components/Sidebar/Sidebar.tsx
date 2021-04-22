159820d156f04357427f4b1ca203806e6b300d0a
zoraaver <zoraaver@gmail.com>
2021-04-22 18:12:07 +0100

    add fade effect when side bar appears/disappears

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,41 +3,43 @@ import { useAppSelector } from "../..";
 import Nav from "react-bootstrap/Nav";
 import "./Sidebar.css";
 import { useHistory } from "react-router";
+import Fade from "react-bootstrap/Fade";
 
 export function Sidebar() {
   const sidebarOpen: boolean = useAppSelector((state) => state.UI.sidebarOpen);
-  const closed = { display: "none" };
   const history = useHistory();
   return (
-    <div className="sidenav" style={sidebarOpen ? {} : closed}>
-      <Nav.Link
-        as="button"
-        className="btn"
-        onClick={() => history.push("/plans/new")}
-      >
-        Create a new plan
-      </Nav.Link>
-      <Nav.Link
-        as="button"
-        className="btn"
-        onClick={() => history.push("/plans")}
-      >
-        Manage my plans
-      </Nav.Link>
-      <Nav.Link
-        as="button"
-        className="btn"
-        onClick={() => history.push("/statistics")}
-      >
-        Statistics
-      </Nav.Link>
-      <Nav.Link
-        as="button"
-        className="btn"
-        onClick={() => history.push("/exercises")}
-      >
-        Exercises
-      </Nav.Link>
-    </div>
+    <Fade in={sidebarOpen}>
+      <div className="sidenav">
+        <Nav.Link
+          as="button"
+          className="btn"
+          onClick={() => history.push("/plans/new")}
+        >
+          Create a new plan
+        </Nav.Link>
+        <Nav.Link
+          as="button"
+          className="btn"
+          onClick={() => history.push("/plans")}
+        >
+          Manage my plans
+        </Nav.Link>
+        <Nav.Link
+          as="button"
+          className="btn"
+          onClick={() => history.push("/statistics")}
+        >
+          Statistics
+        </Nav.Link>
+        <Nav.Link
+          as="button"
+          className="btn"
+          onClick={() => history.push("/exercises")}
+        >
+          Exercises
+        </Nav.Link>
+      </div>
+    </Fade>
   );
 }
