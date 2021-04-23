1893214987f2fa8670f2029ca9cab8825ea13450
zoraaver <zoraaver@gmail.com>
2021-04-23 16:48:28 +0100

    Add statistics page.

---
+++ src/pages/StatisticsPage.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Container from "react-bootstrap/Container";
+
+export function StatisticsPage() {
+  return (
+    <Container className="d-flex flex-column mt-5 align-items-center">
+      <h3 className="mt-3">Statistics</h3>
+    </Container>
+  );
+}
