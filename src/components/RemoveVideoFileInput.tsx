aedc3def20b507461f63914e6c6b78207a1c890e
zoraaver <zoraaver@gmail.com>
2021-04-29 21:01:03 +0100

    Remove video from workout log

---
+++ src/components/RemoveVideoFileInput.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import Button from "react-bootstrap/Button";
+import { useAppDispatch, useAppSelector } from "..";
+import {
+  removeFormVideo,
+  WorkoutLogPosition,
+} from "../slices/workoutLogsSlice";
+
+export function RemoveVideoFileInput({
+  setIndex,
+  exerciseIndex,
+}: WorkoutLogPosition) {
+  const dispatch = useAppDispatch();
+  const formVideo = useAppSelector(
+    (state) =>
+      state.workoutLogs.editWorkoutLog.exercises[exerciseIndex].sets[setIndex]
+        .formVideo
+  );
+  return (
+    <>
+      {formVideo}
+      <Button
+        className="py-0 my-0 ml-3"
+        variant="danger"
+        onClick={() => dispatch(removeFormVideo({ setIndex, exerciseIndex }))}
+      >
+        x
+      </Button>
+    </>
+  );
+}
