dd640463aa0a29bd68ca95ce112e662f79c51ac8
zoraaver <zoraaver@gmail.com>
2021-05-03 20:31:21 +0100

    Add upload progress bar and alert when there is an issue with a file attachment

--- src/pages/GeneratedWorkoutLogPage.tsx
+++ src/pages/GeneratedWorkoutLogPage.tsx
@@ -3,15 +3,18 @@ import { Redirect, useHistory } from "react-router";
 import { useAppDispatch, useAppSelector } from "..";
 import { workoutData } from "../slices/workoutsSlice";
 import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 import {
   clearEditWorkoutLog,
   postWorkoutLog,
   resetSuccess,
+  setFormVideoError,
   setWorkoutId,
   workoutLogData,
 } from "../slices/workoutLogsSlice";
 import { GeneratedWorkoutLogTable } from "../containers/GeneratedWorkoutLogTable";
+import { UploadProgressBar } from "../components/UploadProgressBar";
 
 export function GeneratedWorkoutLogPage() {
   const dispatch = useAppDispatch();
@@ -20,6 +23,12 @@ export function GeneratedWorkoutLogPage() {
   );
   const workoutLog: workoutLogData = useAppSelector(
     (state) => state.workoutLogs.editWorkoutLog
+  );
+  const formVideoError: string | undefined = useAppSelector(
+    (state) => state.workoutLogs.formVideoError
+  );
+  const videoUploadProgress: number = useAppSelector(
+    (state) => state.workoutLogs.videoUploadProgress
   );
   const history = useHistory();
 
@@ -34,6 +43,9 @@ export function GeneratedWorkoutLogPage() {
     setIndex: 0,
     exerciseIndex: 0,
   });
+
+  if (videoUploadProgress)
+    return <UploadProgressBar percentage={videoUploadProgress} />;
   const workoutFinished: boolean =
     positionInWorkout.exerciseIndex >= workout.exercises.length;
 
@@ -47,6 +59,15 @@ export function GeneratedWorkoutLogPage() {
   return (
     <Container className="mt-5 d-flex flex-column justify-content-start align-items-center">
       <h2 className="my-3">Log today's workout:</h2>
+      <Alert
+        dismissible
+        className="my-3"
+        variant="danger"
+        show={!!formVideoError}
+        onClose={() => dispatch(setFormVideoError(undefined))}
+      >
+        {formVideoError}
+      </Alert>
       <GeneratedWorkoutLogTable
         positionInWorkout={positionInWorkout}
         setPositionInWorkout={setPositionInWorkout}
