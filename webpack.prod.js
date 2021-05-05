93f7df0365629144522ee23038237750c695a580
zoraaver <zoraaver@gmail.com>
2021-05-05 17:16:46 +0100

    Add copy plugin to copy over redirects file from public directory to build directory

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,23 @@
 const { webpackCommonConfig } = require("./webpack.common");
 const { merge } = require("webpack-merge");
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 const webpackProdConfig = {
   mode: "production",
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public", "_redirects"),
+          to: path.resolve(__dirname, "build"),
+        },
+      ],
+      options: {
+        concurrency: 100,
+      },
+    }),
+  ],
 };
 
 module.exports = merge(webpackCommonConfig, webpackProdConfig);
