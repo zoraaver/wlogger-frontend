580dff6fca7407002200ffacbcee51690d8b7619
zoraaver <zoraaver@gmail.com>
2021-06-29 17:15:47 +0100

    Add form to add exercise and display list of exercises

--- src/pages/ExercisesPage.tsx
+++ src/pages/ExercisesPage.tsx
@@ -1,10 +1,28 @@
 import * as React from "react";
+import { ExerciseForm } from "../components/ExerciseForm";
 import Container from "react-bootstrap/Container";
+import { useAppDispatch, useAppSelector } from "..";
+import { exerciseData, getExercises } from "../slices/exercisesSlice";
+import { ExerciseItem } from "../components/ExerciseItem";
 
 export function ExercisesPage() {
+  const dispatch = useAppDispatch();
+
+  const exercises: exerciseData[] = useAppSelector(
+    (state) => state.exercises.data
+  );
+
+  React.useEffect(() => {
+    dispatch(getExercises());
+  }, []);
+
   return (
     <Container className="d-flex flex-column mt-5 align-items-center">
-      <h3 className="mt-3">Exercises</h3>
+      <h2 className="mt-3">Exercises</h2>
+      <ExerciseForm />
+      {exercises.map((exercise) => (
+        <ExerciseItem exercise={exercise} key={exercise.name} />
+      ))}
     </Container>
   );
 }
