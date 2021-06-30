71cb3849cef1069fe72a604e09a5f60150479b13
zoraaver <zoraaver@gmail.com>
2021-06-30 18:21:18 +0100

    Export root state type

--- src/index.tsx
+++ src/index.tsx
@@ -28,7 +28,7 @@ const store = configureStore({
   },
 });
 
-type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
